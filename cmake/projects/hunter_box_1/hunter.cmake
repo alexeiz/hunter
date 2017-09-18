a34ffa47cac5687582b31ddbd0d6d37bb7e65dc7
hunterbox <hunterbox@users.noreply.github.com>
2017-09-18 16:25:59 +0300

    Update 'hunter_box_1' to v1.0.1

--- cmake/projects/hunter_box_1/hunter.cmake
+++ cmake/projects/hunter_box_1/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     4fa7fe75629f148a61cedc6ba0bce74f177a6747
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    hunter_box_1
+    VERSION
+    1.0.1
+    URL
+    "https://github.com/hunterbox/hunter_box_1/archive/v1.0.1.tar.gz"
+    SHA1
+    10d046eec6c8b0aabd28bd3d1b99faf6beeb226b
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(hunter_box_1)
 hunter_download(PACKAGE_NAME hunter_box_1)
